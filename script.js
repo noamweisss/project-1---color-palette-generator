2fc64d57c4cdcfc7f3694a5a5eeef77f48d385a2
noam weiss <noamw2703@gmail.com>
2025-06-30 16:38:06 +0300

    Simplified functions logic

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-let red, green, blue;
-let RGB = red + green + blue;
-
-function getRandomInt(min, max) {
+function randomBetween(min = 0, max = 256) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
+  // The maximum is exclusive and the minimum is inclusive
 }
 
-function getRGB() {
-  red = getRandomInt(0, 256).toString(16).padStart(2, '0');
-  green = getRandomInt(0, 256).toString(16).padStart(2, '0');
-  blue = getRandomInt(0, 256).toString(16).padStart(2, '0');
-  RGB = red + green + blue;
-
-  console.log("Generated HEX: #" + RGB);
-
+function randomHexColor() {
+  const toHex = n => n.toString(16).padStart(2, '0');
+  const r = randomBetween();
+  const g = randomBetween();
+  const b = randomBetween();
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
 
 const squares = document.querySelectorAll(".colorSquare");
 
 function generatePalette() {
   squares.forEach(function (square) {
-    getRGB();
-    console.log(RGB);
-    square.style.backgroundColor = "#" + RGB;
+    console.log(randomHexColor());
+    square.style.backgroundColor = randomHexColor();
   });
 }
